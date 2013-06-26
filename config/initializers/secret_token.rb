c23075321ba8bb714232a68116b5f64781105096
Livien Yin <LivienYin@gmail.com>
2013-06-25 18:13:08 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnePlusOne::Application.config.secret_token = 'afb9b71c85a1db63040f2e95debb714ea38c316767de98526401a80b984f7d72719a735a0244e575ea0bad6169c2c44641b9e94ee31f453f8129f0369db272b9'
